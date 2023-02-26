48aeac4ecc094138778a98bf5103577180932106
Alexander Freyr <lexi-freyr@hotmail.com>
2023-02-26 22:25:12 +0000

    preload image

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -8,19 +8,16 @@ export default function ImageGallery({
   const [index, setIndex] = useState(0);
   const [URLs, setURLs] = useState(Array.isArray(urls) ? urls : null);
 
-  function Button({
-    side,
-    length,
-  }: {
-    side: "left" | "right";
-    length: number;
-  }) {
+  function Button({ side, URLs }: { side: "left" | "right"; URLs: string[] }) {
+    const length = URLs.length;
     return (
       <div
         className={"button " + side + "-button"}
-        onClick={(ev) =>
-          setIndex((index + (side == "left" ? -1 : 1) + length) % length)
-        }
+        onClick={(ev) => {
+          const swipeDir = side == "left" ? -1 : 1;
+          new Image().src = URLs[index + swipeDir * 2];
+          setIndex((index + swipeDir + length) % length);
+        }}
       >
         <div className="button-icon">{side == "left" ? "<" : ">"}</div>
       </div>
@@ -29,15 +26,15 @@ export default function ImageGallery({
 
   // localStorage.setItem("imageURLs", JSON.stringify(urls));
 
-  if (urls instanceof Promise) 
+  if (urls instanceof Promise)
     urls.then((v) => setURLs(v)).catch((err) => console.error(err));
   if (URLs === null) return <></>;
   else
     return (
       <div className="ImageGallery">
-        <Button side="left" length={URLs.length}></Button>
+        <Button side="left" URLs={URLs}></Button>
         <img src={URLs[index]}></img>
-        <Button side="right" length={URLs.length}></Button>
+        <Button side="right" URLs={URLs}></Button>
       </div>
     );
 }
